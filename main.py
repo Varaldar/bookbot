f86ad83d3c02061031f5cace93256e8bce361b24
Varaldar <142377833+Varaldar@users.noreply.github.com>
2025-02-13 18:20:13 -0600

    added total word count and individual letter count

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    #print(file_contents)
+    words = file_contents.split() # Split the file contents into a list of words
+    total_words = len(words) # Count the number of words in the list
+    #print(total_words)
+    individual_letters = list(file_contents.lower()) # Split file contents into lowercase words
+    count_dict = {}
+    for i in individual_letters:
+        if i in count_dict:
+            count_dict[i] += 1
+        else: 
+            count_dict[i] = 1       
+    print(count_dict)
+main()
